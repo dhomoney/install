64786737c3ac1a05faa149531ae28d8329392bcd
David Homoney <david@homoney.com>
2023-07-07 11:38:13 -0500

    Updated to download and install neosec.

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://artifacts.neosec.com/installer/node-installer > installer && chmod +x installer && sudo ./installer install yum -username lab1@akamaiwwtapisecuritylab.com -password '&!&wwf5j4$UmP!9x' -source-id 8q26WccZLc1Y2Al7wf1zq4DsmNIDfppi8NDLRPGn -collector-name 'wwt_demo_1'
